01e3794d775cfeec6e5ca77bd297905e77eea3d9
Newlooc <newlooc@gmail.com>
2018-11-15 20:12:17 +0800

    1

--- csv_test.py
+++ csv_test.py
@@ -2,23 +2,13 @@ import csv
 import time
 
 f = open("data/train.csv")
-readers = csv.reader(f)
-
+    readers = csv.reader(f)
 keys = ('AMB_TEMP', 'CH4', 'CO', 'NMHC', 'NO', 'NO2', 'NOx', 'O3', 'PM10', 'PM2.5', 'RAINFALL', 'RH', 'SO2', 'THC', 'WD_HR', 'WIND_DIREC', 'WIND_SPEED', 'WS_HR')
 
-dayOfContent = {}
-components = {}
+item = defaultdict(dict)
 
-for rkey, row in enumerate(readers) :
-    if(rkey > 0) :
-        for cKey, column in enumerate(row) :
-            if(cKey > 1) :
-                timeString = '{} {}'.format(row[0], str(cKey - 2))
-                ts = time.strptime(timeString, '%Y/%m/%d %H')
-                tsKey = time.mktime(ts)
-                components[row[1]] = column
-        dayOfContent[str(int(tsKey))] = components
+item['a']['c'] = '123'
 
-print(dayOfContent)
+print(item)
                 
 
